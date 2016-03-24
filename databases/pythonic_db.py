baf35711dae6682509166fe37e26866a5efb6be3
Alex Kiura <alex.kiura@andela.com>
2016-03-25 00:03:55 +0300

    [Feature] Add users table and create empty setup_table methodd

--- databases/pythonic_db.py
+++ databases/pythonic_db.py
@@ -13,13 +13,30 @@ cars = ((1, 'Audi', 52642),
         (7, 'Range Rove', 52642)
         )
 
+users = ((1, 'Ngeos', 20),
+         (2, 'Tei', 21),
+         (3, 'John', 23),
+         (4, 'Gibbs', 29),
+         (5, 'Niki', 33),
+         (6, 'Meeek', 28),
+         (7, 'Beyonce', 76)
+         )
 con = lite.connect('test.db')
+
+
+def setup_table(cursor, table_name, data, **options):
+    """Create table table_name and add data to columns."""
+    cursor.execute("DROP TABLE IF EXISTS " + table_name)
+    pass
 
 with con:
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS Cars")
+    cur.execute("DROP TABLE IF EXISTS Users")
     cur.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
+    cur.execute("CREATE TABLE Users(Id INT, Name TEXT, Age INT)")
     cur.executemany("INSERT INTO Cars VALUES(?, ?, ?)", cars)
+    cur.executemany("INSERT INTO Users VALUES(?, ?, ?)", users)
 
     data = cur.fetchone()
 
