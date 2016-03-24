74310ecf21036bc35af1ecb54cc163d8edea3e09
Alex Kiura <alex.kiura@andela.com>
2016-03-24 12:44:58 +0300

    [Feature] Create & populate Cars table using executemany command

--- databases/pythonic_db.py
+++ databases/pythonic_db.py
@@ -4,19 +4,22 @@
 
 import sqlite3 as lite
 
+cars = ((1, 'Audi', 52642),
+        (2, 'Jeep', 52643),
+        (3, 'Benz', 52644),
+        (4, 'Koenig', 52645),
+        (5, 'Ferrari', 52646),
+        (6, 'Bugatti', 52647),
+        (7, 'Range Rove', 52642)
+        )
+
 con = lite.connect('test.db')
 
 with con:
     cur = con.cursor()
-    cur.execute("SELECT SQLITE_VERSION()")
+    cur.execute("DROP TABLE IF EXISTS Cars")
     cur.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
-    cur.execute("INSERT INTO Cars VALUES(1, 'Audi', 52642)")
-    cur.execute("INSERT INTO Cars VALUES(2, 'Jeep', 52643)")
-    cur.execute("INSERT INTO Cars VALUES(3, 'Benz', 52644)")
-    cur.execute("INSERT INTO Cars VALUES(4, 'Koenig', 52645)")
-    cur.execute("INSERT INTO Cars VALUES(5, 'Ferrari', 52646)")
-    cur.execute("INSERT INTO Cars VALUES(6, 'Bugatti', 52647)")
-    cur.execute("INSERT INTO Cars VALUES(7, 'Range Rover', 52642)")
+    cur.executemany("INSERT INTO Cars VALUES(?, ?, ?)", cars)
 
     data = cur.fetchone()
 
