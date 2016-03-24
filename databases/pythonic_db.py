a01337c3cb0f0d5afb73759b65cfb9003b2953b9
Alex Kiura <alex.kiura@andela.com>
2016-03-24 12:11:07 +0300

    [Feature] Create & populate Cars table

--- databases/pythonic_db.py
+++ databases/pythonic_db.py
@@ -1,15 +1,23 @@
-#!/usr/bin/python
+"""SQlite3 practice."""
+# !/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import sqlite3 as lite
-import sys
 
 con = lite.connect('test.db')
 
 with con:
     cur = con.cursor()
-    cur.execute('SELECT SQLITE_VERSION()')
+    cur.execute("SELECT SQLITE_VERSION()")
+    cur.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
+    cur.execute("INSERT INTO Cars VALUES(1, 'Audi', 52642)")
+    cur.execute("INSERT INTO Cars VALUES(2, 'Jeep', 52643)")
+    cur.execute("INSERT INTO Cars VALUES(3, 'Benz', 52644)")
+    cur.execute("INSERT INTO Cars VALUES(4, 'Koenig', 52645)")
+    cur.execute("INSERT INTO Cars VALUES(5, 'Ferrari', 52646)")
+    cur.execute("INSERT INTO Cars VALUES(6, 'Bugatti', 52647)")
+    cur.execute("INSERT INTO Cars VALUES(7, 'Range Rover', 52642)")
 
     data = cur.fetchone()
 
-    print 'SQLite version: %s' % data
+    print "SQLite version: %s" % data
